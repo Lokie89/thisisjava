8d01f6433dd3e9163e56063ccf0abed8878dc9b9
Lokie89 <traeuman@gmail.com>
2020-01-28 23:08:02 +0900

    20200128

--- src/chapter1/CharExample.java
+++ src/chapter1/CharExample.java
@@ -18,3 +18,20 @@ public class CharExample {
         System.out.println(c6);
     }
 }
+
+class FromIntToFloat {
+    public static void main(String[] args) {
+        int num1 = 123456780;
+        int num2 = 123456780;
+
+        float num3 = num2;
+        System.out.println(num1+" "+num2+" "+num3);
+        num2 = (int) num3;
+        int result = num1 - num2;
+        System.out.println(result);
+
+        byte b = 10;
+        float  f = 2.5F;
+        double d = 2.5;
+    }
+}
